b073b40e787dc9d4d5e09e0889410e95a29b9a55
Barnaby Colby <barnaby.colby@gmail.com>
2016-06-10 22:26:15 +0000

    Added validation checks to verify the version number before use, as well as prettying up the script with some verbose outputs.

--- phantomjsdownload.sh
+++ phantomjsdownload.sh
@@ -2,7 +2,17 @@
 
 # Prints the latest version of phantomjs available from archlinuxarm
 printLatestVersion() {
-    curl 'http://mirror.archlinuxarm.org/armv7h/community/' -L -s | grep 'phantomjs-.*-armv7h.pkg.tar.xz<' | sed -e 's/^.*<a href="phantomjs-//' | sed -e 's/-armv7h.*$//'
+    local version=$(curl 'http://mirror.archlinuxarm.org/armv7h/community/' -L -s | grep 'phantomjs-.*-armv7h.pkg.tar.xz<' | sed -e 's/^.*<a href="phantomjs-//' | sed -e 's/-armv7h.*$//')
+    
+    # Check that the version number is valid
+    versionRegex="^[0-9]+\.[0-9]+\.[0-9]+(\$|-[0-9]+)"
+    echo "$(echo "$version" | grep -Eq "$versionRegex")"
+    if [ -z "$version" ] || ! echo "$version" | grep -Eq "$versionRegex" ;then
+        echo "The version number scraped from the ALARM mirror was invalid: $version"
+        exit 1
+    fi
+
+    echo $version
 }
 
 # Check to see whether we already have it archived
@@ -18,5 +28,7 @@ download() {
     rm phantomjs-2.1.1-3-armv7h.pkg.tar.xz
 }
 
+echo -n "Finding the latest version of phantomjs available..."
 latestVersion=$(printLatestVersion)
+echo "Done."
 echo $latestVersion
