b437cc339da0a291e611b4e84e28b280ce1bb79d
Barnaby Colby <barnaby.colby@gmail.com>
2016-07-04 21:34:29 +0000

    Cleaned up script output, which closes #4.

--- phantomjsdownload.sh
+++ phantomjsdownload.sh
@@ -12,7 +12,7 @@ printLatestVersion() {
     versionRegex="^[0-9]+\.[0-9]+\.[0-9]+(\$|-[0-9]+)"
     echo "$(echo "$version" | grep -Eq "$versionRegex")"
     if [ -z "$version" ] || ! echo "$version" | grep -Eq "$versionRegex" ;then
-        echo "The version number scraped from the ALARM ${architecture} mirror was invalid: $version"
+        echo "- The version number scraped from the ALARM ${architecture} mirror was invalid: $version"
         return 1
     fi
 
@@ -57,35 +57,47 @@ download() {
 
     # Download and extract the Arch Linux ARM package
     local pkgFilename="phantomjs-${alarmVersion}-${architecture}.pkg.tar.xz"
-    wget "http://mirror.archlinuxarm.org/${architecture}/community/${pkgFilename}" -O ${tmp}/${pkgFilename}
+    echo -n "- Downloading the Arch Linux ARM package..."
+    wget --quiet "http://mirror.archlinuxarm.org/${architecture}/community/${pkgFilename}" -O ${tmp}/${pkgFilename}
+    echo "Done."
 
     # Download and extract the bitbucket package (for the changelog and readme), if it is not already downloaded
     bitbucketFilenameNoExtension="phantomjs-${version}-linux-x86_64"
     bitbucketFilename="${bitbucketFilenameNoExtension}.tar.bz2"
     bitbucketFilePath="${tmpRoot}/${bitbucketFilename}"
     if [ ! -f ${bitbucketFilePath} ]; then
-        wget "https://bitbucket.org/ariya/phantomjs/downloads/${bitbucketFilename}" -O ${bitbucketFilePath}
+        echo -n "- Downloading the official x86 package (for README and ChangeLog)..."
+        wget --quiet "https://bitbucket.org/ariya/phantomjs/downloads/${bitbucketFilename}" -O ${bitbucketFilePath}
+        echo "Done."
     fi
 
     # Construct the phantomjs output folder by extracting the required components to the appropriate directory
-    tar xvJf ${tmp}/${pkgFilename} -C ${outputDirPath}/ usr/bin --strip-components=1
-    tar xvJf ${tmp}/${pkgFilename} -C ${outputDirPath}/ usr/share/phantomjs/examples --strip-components=3
-    tar xvJf ${tmp}/${pkgFilename} -C ${outputDirPath}/ usr/share/licenses/phantomjs --strip-components=4
-    tar xvjf ${bitbucketFilePath} -C ${outputDirPath}/ ${bitbucketFilenameNoExtension}/README.md --strip-components=1
-    tar xvjf ${bitbucketFilePath} -C ${outputDirPath}/ ${bitbucketFilenameNoExtension}/ChangeLog --strip-components=1
+    echo -n "- Constructing the output directory by extracting the required components..."
+    tar xJf ${tmp}/${pkgFilename} -C ${outputDirPath}/ usr/bin --strip-components=1
+    tar xJf ${tmp}/${pkgFilename} -C ${outputDirPath}/ usr/share/phantomjs/examples --strip-components=3
+    tar xJf ${tmp}/${pkgFilename} -C ${outputDirPath}/ usr/share/licenses/phantomjs --strip-components=4
+    tar xjf ${bitbucketFilePath} -C ${outputDirPath}/ ${bitbucketFilenameNoExtension}/README.md --strip-components=1
+    tar xjf ${bitbucketFilePath} -C ${outputDirPath}/ ${bitbucketFilenameNoExtension}/ChangeLog --strip-components=1
+    echo "Done."
 
     # Compress the output folder
     local outputArchiveExtension="tar.bz2"
     local outputArchive="phantomjs-${alarmVersion}-linux-${architecture}.${outputArchiveExtension}"
     local outputFile="${tmp}/${outputArchive}"
-    tar cvjf ${outputFile} --directory=${tmp} ${outputDir}
+    echo -n "- Compressing the output directory..."
+    tar cjf ${outputFile} --directory=${tmp} ${outputDir}
+    echo "Done."
 
     # Move the output archive to the appropriate location
+    echo -n "- Moving the output archive to the final destination..."
     mv ${outputFile} ${downloadLocation}
+    echo "Done."
 
     # Symbolically link the latest phantomjs version to the latest ALARM version
+    echo -n "- Creating symlink file without the extra Arch Linux ARM versioning..."
     local outputFileNoAlarmVersioning="${outputDir}.${outputArchiveExtension}"
     ln -sf ${downloadLocation}/${outputArchive} ${downloadLocation}/${outputFileNoAlarmVersioning}
+    echo "Done."
 }
 
 # If the latest version has not already been downloaded, it is downloaded and packaged for use
@@ -94,8 +106,10 @@ downloadAndPackage() {
     local architecture=$1
     local tmp=$2
 
+    echo "${architecture}"
+
     # Get the latest version, ensuring that we check for errors
-    echo -n "Finding the latest version of phantomjs available..."
+    echo -n "- Finding the latest version of phantomjs available..."
     latestAlarmVersionWithSpaces="$(printLatestVersion ${architecture})"
     if [ $? != 0 ]; then
         echo "${latestAlarmVersionWithSpaces}"
@@ -105,17 +119,14 @@ downloadAndPackage() {
     # tr -d removes all whitespace, in particular, this is used to remove the leading newline added by echo
     latestAlarmVersion="$(echo ${latestAlarmVersionWithSpaces} | tr -d '[[:space:]]')"
     echo "Done."
-    echo "${latestAlarmVersion}"
 
-    echo -n "Checking to see whether the latest version has already been downloaded..."
+    echo -n "- Checking to see whether the latest version has already been downloaded..."
     if alreadyDownloaded "${architecture}" "${latestAlarmVersion}"; then
         echo "Done."
-        echo "The latest version of phantomjs ($latestAlarmVersion) for the ${architecture} architecture has already been downloaded."
+        echo "- The latest version of phantomjs ($latestAlarmVersion) for the ${architecture} architecture has already been downloaded."
     else
         echo "Done."
-        echo -n "Downloading latest version..."
         download "${architecture}" "${latestAlarmVersion}" "${tmp}"
-        echo "Done."
     fi
 }
 
